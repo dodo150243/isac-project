c1cd02751c775ff18f21d961a3ae2a83f6d5d42d
ntpoom9 <80139353+ntpoom9@users.noreply.github.com>
2021-09-14 00:02:15 +0700

    add

---
+++ Isac_old/Isac_/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'codeigniter4/translations' => array(
+            'pretty_version' => 'v4.1.4',
+            'version' => '4.1.4.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../codeigniter4/translations',
+            'aliases' => array(),
+            'reference' => 'cfd7b817167d5f92b61f92459ffe533cfa280f38',
+            'dev_requirement' => false,
+        ),
+    ),
+);
