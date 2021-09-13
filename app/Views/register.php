4048528ff13cfce27f3e4b8c234b31f47a558ef2
ntpoom9 <80139353+ntpoom9@users.noreply.github.com>
2021-09-13 22:00:56 +0700

    new title register

--- app/Views/register.php
+++ app/Views/register.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register 01</title>
+    <title>Register 02</title>
     <!-- Boostrap -->
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
