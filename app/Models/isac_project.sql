f0966aec7b8b4ec34b4ab150d2b695bcac72750d
dodo150243 <tirawat330@gmail.com>
2021-09-19 22:57:29 +0700

    add

--- app/Models/isac_project.sql
+++ app/Models/isac_project.sql
@@ -1,6 +1,36 @@
 drop DATABASE isac_project;
 CREATE DATABASE isac_project CHARACTER SET utf8  COLLATE  utf8_general_ci;
 
+
+
+CREATE TABLE alumni_user(
+	alumni_id int(11) NOT NULL AUTO_INCREMENT,
+    stu_id int(11) NOT NULL,
+    password varchar(250) NOT NULL,
+    name_prefix varchar(250) NOT NULL,         
+    alumni_fname varchar(250) NOT NULL,       
+    alumni_lname  varchar(250) NOT NULL,     
+    alumni_fname_eng   varchar(250) NOT NULL,   
+    alumni_lname_eng    varchar(250) NOT NULL,     
+    alumni_code varchar(13) NOT NULL, 
+    sex varchar(10)NOT NULL,    
+    d_m_y_birth date NOT NULL, 
+    nationality  varchar(250) NOT NULL,        
+    religion      varchar(250) NOT NULL,    
+    blood_type varchar(250) NOT NULL,
+    -- address_id int(11) NOT NULL,
+    -- social_id  int(11) NOT NULL,
+    -- education_id int(11) NOT NULL,
+    -- job_id int(11) NOT NULL,
+    -- img_id int(11) NOT NULL,
+    PRIMARY KEY(alumni_id)
+	
+	-- FOREIGN KEY(social_id) REFERENCES tbl_social(social_id),
+	-- FOREIGN KEY(education_id) REFERENCES tbl_education(education_id),
+    -- FOREIGN KEY(job_id) REFERENCES tbl_job(job_id),
+    -- FOREIGN KEY(img_id) REFERENCES tbl_img(img_id)
+);
+
 CREATE TABLE tbl_address(
 address_id int(11) NOT NULL AUTO_INCREMENT,
 numhome_b varchar(250) NOT NULL,
@@ -15,7 +45,8 @@ sub_district_c  varchar(250) NOT NULL,
 district_c      varchar(250) NOT NULL,  
 province_c varchar(250) NOT NULL,
 zipcode_c int(5) NOT NULL,
- PRIMARY KEY(address_id)
+ PRIMARY KEY(address_id),
+ FOREIGN KEY(address_id) REFERENCES alumni_user(alumni_id)
 );
 
 CREATE TABLE tbl_social(
@@ -23,7 +54,8 @@ social_id int(11) NOT NULL AUTO_INCREMENT,
 facebook varchar(250) NOT NULL,
 line varchar(250) NOT NULL,
 phone_number int(10) NOT NULL,
- PRIMARY KEY(social_id)
+ PRIMARY KEY(social_id),
+ FOREIGN KEY(social_id) REFERENCES alumni_user(alumni_id)
 );
 
 CREATE TABLE tbl_education(
@@ -34,7 +66,8 @@ year_finish int(4) NOT NULL,
 faculty varchar(250) NOT NULL,
 major varchar(250) NOT NULL,
 section varchar(250) NOT NULL,
- PRIMARY KEY(education_id)
+ PRIMARY KEY(education_id),
+ FOREIGN KEY(education_id) REFERENCES alumni_user(alumni_id)
 );
 
 CREATE TABLE tbl_job(
@@ -44,40 +77,16 @@ start_date date NOT NULL,
 company_name varchar(250) NOT NULL,
 company_address varchar(250) NOT NULL,
 contact int(10) NOT NULL,
- PRIMARY KEY(job_id)
+ PRIMARY KEY(job_id),
+ FOREIGN KEY(job_id) REFERENCES alumni_user(alumni_id)
 
 );
 
 CREATE TABLE tbl_img(
 img_id int(11) NOT NULL AUTO_INCREMENT,
 imges varchar(250) NOT NULL,
- PRIMARY KEY(img_id)
+ PRIMARY KEY(img_id),
+  FOREIGN KEY(img_id) REFERENCES alumni_user(alumni_id)
 
 );
 
-CREATE TABLE alumni_user(
-	alumni_id int(11) NOT NULL AUTO_INCREMENT,
-    stu_id int(11) NOT NULL,
-    password varchar(250) NOT NULL,
-    name_prefix varchar(250) NOT NULL,         
-    alumni_fname varchar(250) NOT NULL,       
-    alumni_lname  varchar(250) NOT NULL,     
-    alumni_fname_eng   varchar(250) NOT NULL,   
-    alumni_lname_eng    varchar(250) NOT NULL,     
-    alumni_code varchar(13) NOT NULL,         
-    d_m_y_birth date NOT NULL, 
-    nationality  varchar(250) NOT NULL,        
-    religion      varchar(250) NOT NULL,    
-    blood_type varchar(250) NOT NULL,
-    address_id int(11) NOT NULL,
-    social_id  int(11) NOT NULL,
-    education_id int(11) NOT NULL,
-    job_id int(11) NOT NULL,
-    img_id int(11) NOT NULL,
-    PRIMARY KEY(alumni_id),
-	FOREIGN KEY(address_id) REFERENCES tbl_address(address_id),
-	FOREIGN KEY(social_id) REFERENCES tbl_social(social_id),
-	FOREIGN KEY(education_id) REFERENCES tbl_education(education_id),
-    FOREIGN KEY(job_id) REFERENCES tbl_job(job_id),
-    FOREIGN KEY(img_id) REFERENCES tbl_img(img_id)
-);
